30fc3156d0149f77f831b8a7a7dbba6febf1f2e1
Paturages <Paturages@users.noreply.github.com>
2019-05-27 10:18:45 +0200

    Remove console.log

--- randomize.js
+++ randomize.js
@@ -1,7 +1,6 @@
 if (location.hash) {
   var parts = location.hash.slice(1).split("&");
   var rand = Math.random() * parts.length >> 0;
-  console.log(parts, rand, parts[rand]);
   try {
     location = atob(parts[rand]);
   } catch (e) {
